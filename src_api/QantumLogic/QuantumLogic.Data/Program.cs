f83e2f0a645e7e6a5327bc01f04533526330b98b
toto <ultramarine256@gmail.com>
2017-07-31 23:00:34 +0300

    Added architecture layers

---
+++ src_api/QantumLogic/QuantumLogic.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuantumLogic.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
