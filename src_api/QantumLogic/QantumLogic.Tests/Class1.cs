3cd0b3a1d0ee9565f42d49388d9d8e8a39a83eed
Evgeny Platonov <ultramarine256@gmail.com>
2017-07-31 13:56:26 +0300

    Added API sources

---
+++ src_api/QantumLogic/QantumLogic.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace QantumLogic.Tests
+{
+    public class Class1
+    {
+    }
+
+    [TestFixture]
+    public sealed class TypeToTestTests
+    {
+        [TestAttribute]
+        public void Test()
+        {
+            
+        }
+    }
+}
