d633876bc839739416eb00b8d0de4c1f66300360
Evgeny Platonov <ultramarine256@gmail.com>
2017-08-24 21:46:14 +0300

    Added deployment script;

---
+++ src_widget/deploy.ps1
@@ -0,0 +1,10 @@
+$msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+
+$msdeployArguments = '-verb:sync',
+		'-source:contentPath="C:\Data\Sources\Quantum\src_widget\build"',
+		'-dest:contentPath="dev-widget-quantumlogic",wmsvc=dev-widget-quantumlogic.scm.azurewebsites.net:443/msdeploy.axd?site=dev-widget-quantumlogic,userName=$dev-widget-quantumlogic,password=uJ6AbPS13eml3mJQDvT1uJn4oMJL2sAfmvPzLFJdlHqmNKuntCzKGEpECz3b',
+		'-skip:Directory="node_modules"',
+		# '-skip:Directory="uploads"',
+		'-AllowUntrusted'
+		
+& $msdeploy $msdeployArguments
