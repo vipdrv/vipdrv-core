134bd148c0c8074ff677e6fe3052f05cf6ab4a80
Andrey Nevzorov <ya.kobos@yandex.ru>
2017-09-03 18:08:54 +0300

    Config fixes.

--- src_admin/src/app/pages/sites/sites.component.ts
+++ src_admin/src/app/pages/sites/sites.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SiteEntity } from './../../entities/index';
+import { ISiteApiService, SiteApiService, GetAllResponse } from './../../services/serverApi/index';
 @Component({
     selector: 'sites',
     styleUrls: ['./sites.scss'],
     templateUrl: './sites.html',
 })
-export class SitesComponent {
-    constructor() { }
+export class SitesComponent implements OnInit{
+    /// fields
+    protected totalCount: number;
+    protected items: Array<SiteEntity>;
+    /// injected dependencies
+    protected siteApiService: ISiteApiService;
+    /// ctor
+    constructor(siteApiService: SiteApiService) {
+        this.siteApiService = siteApiService;
+    }
+    /// methods
+    ngOnInit(): void {
+        this.loadData();
+    }
+    /// helpers
+    protected loadData(): Promise<void> {
+        let self = this;
+        let loadItemsPromise = self.siteApiService
+            .getAll(0, 25, 'name asc', null)
+            .then(function (response: GetAllResponse<SiteEntity>): void {
+                self.totalCount = response.totalCount;
+                self.items = response.items;
+            });
+        return loadItemsPromise;
+    }
 }
--- src_admin/src/app/theme/components/baAmChart/baAmChart.component.ts
+++ src_admin/src/app/theme/components/baAmChart/baAmChart.component.ts
@@ -2,12 +2,12 @@ import {Component, ViewChild, Input, Output, ElementRef, EventEmitter} from '@an
 
 import {BaThemePreloader} from '../../../theme/services';
 
-import 'amcharts3';
-import 'amcharts3/amcharts/plugins/responsive/responsive.js';
-import 'amcharts3/amcharts/serial.js';
-
-import 'ammap3';
-import 'ammap3/ammap/maps/js/worldLow';
+// import 'amcharts3';
+// import 'amcharts3/amcharts/plugins/responsive/responsive.js';
+// import 'amcharts3/amcharts/serial.js';
+//
+// import 'ammap3';
+// import 'ammap3/ammap/maps/js/worldLow';
 
 
 import {BaAmChartThemeService} from './baAmChartTheme.service';
