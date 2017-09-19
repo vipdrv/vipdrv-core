40710fac07e8c89bcc11bab6d50f2c9003acc3bc
Evgeny Platonov <ultramarine256@gmail.com>
2017-09-19 19:48:27 +0300

    Added web.config to deployment script;

--- src_admin/deploy.ps1
+++ src_admin/deploy.ps1
@@ -1,10 +1,13 @@
 $msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 
-$msdeployArguments = '-verb:sync',
-		'-source:contentPath="C:\Data\Sources\Quantum\src_admin\dist"',
-		'-dest:contentPath="dev-admin-quantumlogic",wmsvc=dev-admin-quantumlogic.scm.azurewebsites.net:443/msdeploy.axd?site=dev-admin-quantumlogic,userName=$dev-admin-quantumlogic,password=yXg9MoxkHXKEC1sHqYnXPrxfhbsahGWWkeYzgaTnoXcSLrY7PkRrfaEwG57F',
+$msdeployAdminArguments = '-verb:sync',
+		'-source:contentPath="C:\Data\Sources\Quantum\src_admin\dist"','-dest:contentPath="dev-admin-quantumlogic",wmsvc=dev-admin-quantumlogic.scm.azurewebsites.net:443/msdeploy.axd?site=dev-admin-quantumlogic,userName=$dev-admin-quantumlogic,password=yXg9MoxkHXKEC1sHqYnXPrxfhbsahGWWkeYzgaTnoXcSLrY7PkRrfaEwG57F',
 		'-skip:Directory="node_modules"',
-		# '-skip:Directory="uploads"',
+		'-AllowUntrusted'
+
+$msdeployWebConfigArguments = '-verb:sync',
+		'-source:contentPath="C:\Data\Sources\Quantum\src_admin\web.config"','-dest:contentPath="dev-admin-quantumlogic\web.config",wmsvc=dev-admin-quantumlogic.scm.azurewebsites.net:443/msdeploy.axd?site=dev-admin-quantumlogic,userName=$dev-admin-quantumlogic,password=yXg9MoxkHXKEC1sHqYnXPrxfhbsahGWWkeYzgaTnoXcSLrY7PkRrfaEwG57F',
 		'-AllowUntrusted'
 		
-& $msdeploy $msdeployArguments
+& $msdeploy $msdeployAdminArguments
+& $msdeploy $msdeployWebConfigArguments
