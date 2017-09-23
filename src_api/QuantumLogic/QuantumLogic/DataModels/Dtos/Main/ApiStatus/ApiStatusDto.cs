0d876900735f0aba14c01c334ceda22724e305b5
Evgeny Platonov <ultramarine256@gmail.com>
2017-09-23 18:48:33 +0300

    Added ApiStatus controller

---
+++ src_api/QuantumLogic/QuantumLogic/DataModels/Dtos/Main/ApiStatus/ApiStatusDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLogic.WebApi.DataModels.Dtos.Main.ApiStatus
+{
+    public class ApiStatusDto
+    {
+        public string Assembly { get; set; }
+        public string Configuration { get; set; }
+
+        public ApiStatusDto(string aseembly, string configuration)
+        {
+            Assembly = aseembly;
+            Configuration = configuration;
+        }
+    }
+}
--- src_widget/src/app/services/api.service.js
+++ src_widget/src/app/services/api.service.js
@@ -3,13 +3,7 @@
     angular.module('myApp')
         .service('api', function ($http, $q, apiBaseUrl, siteId) {
 
-            this.submitForm = _submitForm;
-            this.loadExperts = _loadExperts;
-            this.loadBeverages = _loadBeverages;
-            this.loadRoads = _loadRoads;
-            this.loadOpenHours = _loadOpenHours;
-
-            function _loadOpenHours() {
+            this.loadOpenHours = function() {
                 var deferred = $q.defer();
                 var url = apiBaseUrl + '/sites/' + siteId + '/open-hours';
 
@@ -21,63 +15,43 @@
                 });
 
                 return deferred.promise;
-            }
+            };
 
-            function _loadRoads() {
-                var deferred = $q.defer();
-                var url = apiBaseUrl + '/sites/' + siteId + '/routes';
+            this.retrieveExperts = function() {
+                var req = {
+                    method: 'POST',
+                    url: apiBaseUrl + 'https://app14.gunnebocloud.com/smartbusiness.auth/connect/token',
+                    headers: {
+                        'cache-control': 'no-cache',
+                        'content-type': 'application/x-www-form-urlencoded',
+                        'authorization': 'Basic ' + helisSecretToken
+                    },
+                    data: {
+                        grant_type: 'password',
+                        username: login,
+                        password: password,
+                        scope: 'SmartBusinessRestApi openid offline_access',
+                    }
+                };
 
-                $http.get(url).then(function (response) {
-                    var json = response.data;
-                    deferred.resolve(json);
-                }, function (err) {
-                    deferred.reject(err);
+                req.transformRequest = function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                };
+
+                var promise = $http(req).then(function (responce) {
+                    if (responce.data.access_token && responce.data.access_token) {
+                        self.updateHeliosServerToken(responce.data.access_token, responce.data.refresh_token);
+                        return responce.data;
+                    }
+                    return false;
+                }, function () {
                 });
 
-                return deferred.promise;
-            }
+                return promise;
+            };
 
-            function _loadBeverages() {
-                var deferred = $q.defer();
-                var url = apiBaseUrl + '/sites/' + siteId + '/beverages';
-
-                $http.get(url).then(function (response) {
-                    var json = response.data;
-                    deferred.resolve(json);
-                }, function (err) {
-
-                    deferred.reject(err);
-                });
-
-                return deferred.promise;
-            }
-
-            function _loadExperts() {
-                var deferred = $q.defer();
-                var url = apiBaseUrl + '/sites/' + siteId + '/experts';
-
-                $http.get(url).then(function (response) {
-                    var json = response.data;
-                    deferred.resolve(json);
-                }, function (err) {
-                    deferred.reject(err);
-                });
-
-                return deferred.promise;
-            }
-
-            function _submitForm(formData) {
-                var deferred = $q.defer();
-                var url = apiBaseUrl + '/sites/' + siteId + '/SubmitForm';
-
-                $http.post(url, formData).then(function (response) {
-                    var json = response.data;
-                    deferred.resolve(json);
-                }, function (err) {
-                    deferred.reject(err);
-                });
-
-                return deferred.promise;
-            }
         });
 })();
