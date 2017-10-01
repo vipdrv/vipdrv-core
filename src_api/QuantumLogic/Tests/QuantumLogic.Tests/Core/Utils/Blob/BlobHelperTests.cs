fc4c9ccc71154b60108d0a91aea446ef62a982f2
Evgeny Platonov <ultramarine256@gmail.com>
2017-10-01 19:58:06 +0300

    Added project for Tests; Add IBlobHelper interface;

---
+++ src_api/QuantumLogic/Tests/QuantumLogic.Tests/Core/Utils/Blob/BlobHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace QuantumLogic.Tests.Core.Utils.Blob
+{
+    [TestFixture]
+    public sealed class BlobHelperTests
+    {
+        [Test]
+        public void SaveFileToBlob__SlouldWork()
+        {
+
+            // Testsing logic goes here
+
+
+
+        }
+    }
+}
