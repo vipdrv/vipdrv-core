6ec1c1d421de6da16b20f92ec9b1e6c9a8a84e59
Evgeny Platonov <ultramarine256@gmail.com>
2017-10-02 19:39:29 +0300

    Created Booking controller

---
+++ src_api/QuantumLogic/QuantumLogic/Controllers/Widget/BookingController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuantumLogic.WebApi.DataModels.Requests.Widget.Booking;
+
+namespace QuantumLogic.WebApi.Controllers.Widget
+{
+    [Route("api/booking")]
+    public class BookingController
+    {
+        [HttpPost("complete-booking")]
+        public Task<string> CompleteBooking(long siteId, [FromBody]CompleteBookingRequest request)
+        {
+            // TODO: send email
+            // TODO: update leads table
+            
+            return Task.FromResult("asd");
+        }
+    }
+}
---
+++ src_api/QuantumLogic/QuantumLogic/DataModels/Requests/Widget/Booking/CompleteBookingRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLogic.WebApi.DataModels.Requests.Widget.Booking
+{
+    public class CompleteBookingRequest
+    {
+        public BookingUser BookingUser { get; set; }
+        public BookingDateTime BookingDateTime { get; set; }
+        public BookingExpert BookingExpert { get; set;}
+        public BookingBeverage BookingBeverage { get; set; }
+        public BookingRoad BookingRoad { get; set; }
+        public BookingCar BookingCar { get; set; }
+
+    }
+
+    public class BookingUser
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool AllowToUsePhone { get; set; }
+        public bool AllowToUseEmail { get; set; }
+    }
+
+    public class BookingDateTime
+    {
+        public DateTime DateTime { get; set; }
+    }
+
+    public class BookingExpert
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BookingBeverage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BookingRoad
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BookingCar
+    {
+        public string CarImageUrl { get; set; }
+        public string Title { get; set; }
+    }
+}
