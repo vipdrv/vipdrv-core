265c307a84a3b9279784903a713f0fdabbd282f8
Evgeny Platonov <ultramarine256@gmail.com>
2017-10-02 16:51:00 +0300

    Added ContentManager;

---
+++ src_api/QuantumLogic/QuantumLogic.Core/Utils/ContentManager/BlobProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace QuantumLogic.Core.Utils.ContentManager
+{
+    public class BlobProvider : IContentManager
+    {
+        private readonly CloudBlobClient _blobClient;
+
+        public BlobProvider()
+        {
+            // TODO: move storage credentials to config file
+            var storageAccount = new CloudStorageAccount(
+                new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(
+                    "generalstandart256",
+                    "ynoR7rDt9T1VRXhpuxfFLg70ySvHOSi6Eq6pOEyqacHyX4YkmLuoZuB66gjhn2b2xrvfoRtbe+iNXfeeSnmePg=="), true);
+            _blobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task<Uri> SaveFileToStorage(FileStream fileStream, string fileName, string contentType)
+        {
+            CloudBlobContainer container = _blobClient.GetContainerReference("image-container");
+
+            // Get a reference to a blob
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            blockBlob.Properties.ContentType = contentType;
+
+            await blockBlob.UploadFromStreamAsync(fileStream);
+            
+            return blockBlob.Uri;
+        }
+    }
+}
---
+++ src_api/QuantumLogic/Tests/QuantumLogic.Tests/Core/Utils/ContentManager/ContentManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using QuantumLogic.Core.Utils.ContentManager;
+
+namespace QuantumLogic.Tests.Core.Utils.ContentManager
+{
+    [TestFixture]
+    public sealed class ContentManagerTests
+    {
+        [Test]
+        [Ignore("Real filesystem, Real storage")]
+        public async Task SaveFileToBlob__SlouldWork()
+        {
+            IContentManager blobProvider = new BlobProvider();
+
+            var fileStream = System.IO.File.OpenRead(@"C:\Users\Ultramarine\Google Drive\Media\Pictures\Test\29312940.jpeg");
+            var fileName = "29312940.jpeg";
+            var contentType = "image/jpg";
+
+            var imageUrl = await blobProvider.SaveFileToStorage(fileStream, fileName, contentType);
+
+            Assert.AreEqual(imageUrl, $"https://generalstandart256.blob.core.windows.net/image-container/{fileName}");
+        }
+    }
+}
