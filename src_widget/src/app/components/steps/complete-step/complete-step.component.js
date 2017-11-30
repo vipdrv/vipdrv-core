85624e616726b17a0b419f47c0c0a60885c8fa28
Evgeny Platonov <ultramarine256@gmail.com>
2017-11-30 23:23:13 +0200

    Fixed widget cards height; Fixed widget sidebar car title;

--- src_widget/src/app/components/steps/complete-step/complete-step.component.js
+++ src_widget/src/app/components/steps/complete-step/complete-step.component.js
@@ -2,9 +2,7 @@
     angular.module('myApp')
         .component('tdCompleteStep', {
             controller: function () {
-
                 var self = this;
-
             },
             templateUrl: 'src/app/components/steps/complete-step/complete-step.tpl.html',
             bindings: {
