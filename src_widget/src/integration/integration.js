36911ecdda93eebad5090afde809efd1c6117fa9
Evgeny Platonov <ultramarine256@gmail.com>
2017-12-01 00:51:12 +0200

    Added hash to iframe url;

--- src_widget/src/integration/integration.js
+++ src_widget/src/integration/integration.js
@@ -8,7 +8,8 @@ var TestDrive = TestDrive || (function () {
     var _WidgetUrl = '%widgetUrl%';
 
     var _appendTestDriveFrame = function (vin, img, title, engine, year, colour, transmission, fuel) {
-        var url = `${_WidgetUrl}?site_id=${_SiteId}&vin=${vin}&imageUrl=${img}&title=${title}&engine=${engine}&year=${year}&colour=${colour}&transmission=${transmission}&fuel=${fuel}`;
+        var hash = Math.random().toString(36).substring(7);
+        var url = `${_WidgetUrl}?site_id=${_SiteId}&vin=${vin}&imageUrl=${img}&title=${title}&engine=${engine}&year=${year}&colour=${colour}&transmission=${transmission}&fuel=${fuel}&hash=${hash}`;
 
         var html =
             `<div class="test-drive__content">
