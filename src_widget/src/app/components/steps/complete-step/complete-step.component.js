c3557c64c53d965b212f5e924d046337e013b3b3
Ave <Maria>
2017-12-04 21:02:41 +0200

    Started complete booking details.

--- src_widget/src/app/components/steps/complete-step/complete-step.component.js
+++ src_widget/src/app/components/steps/complete-step/complete-step.component.js
@@ -3,6 +3,14 @@
         .component('tdCompleteStep', {
             controller: function () {
                 var self = this;
+
+                self.startDate = '2017-12-18 11:00';
+                self.endDate = '2017-12-18 13:00';
+                self.eventTitle = 'VIP Test Drive';
+                self.eventDescription = 'Description of the event';
+
+
+
             },
             templateUrl: 'src/app/components/steps/complete-step/complete-step.tpl.html',
             bindings: {
