e80763e0e351acbaa8432df1c67ce810f56fcb89
Ave Maria <asdf@mail.com>
2017-12-06 19:03:32 +0200

    Changed date to USA format 12/06/17; fixed slicing issues for last step on Safari;

--- src_widget/src/app/components/steps/complete-step/complete-step.component.js
+++ src_widget/src/app/components/steps/complete-step/complete-step.component.js
@@ -1,22 +1,39 @@
 (function () {
+
+    angular.module('myApp')
+        .filter('usaDateFormat', function () {
+            return function (dateStr) {
+                dateStr = dateStr || '';
+                var chunks = dateStr.split("-");
+
+                var out = chunks[1] + '/' + chunks[2] + '/' + chunks[0].slice(-2);
+
+                return out;
+            };
+        });
+
     angular.module('myApp')
         .component('tdCompleteStep', {
             controller: function ($timeout, $window) {
                 var self = this;
 
-                self.$onInit = function() {
+                // =======================================================================//
+                // Calendar Event                                                         //
+                // =======================================================================//
+
+                self.$onInit = function () {
                     var my_awesome_script = $window.document.createElement('script');
-                    my_awesome_script.setAttribute('src','https://addevent.com/libs/atc/1.6.1/atc.min.js');
+                    my_awesome_script.setAttribute('src', 'https://addevent.com/libs/atc/1.6.1/atc.min.js');
 
                     $window.document.head.appendChild(my_awesome_script);
 
-                    $window.addeventasync = function(){
+                    $window.addeventasync = function () {
                         addeventatc.settings({
-                            appleical  : {show:true, text:"Apple Calendar"},
-                            google     : {show:true, text:"Google <em>(online)</em>"},
-                            outlook    : {show:true, text:"Outlook"},
-                            outlookcom : {show:true, text:"Outlook.com <em>(online)</em>"},
-                            yahoo      : {show:false, text:"Yahoo <em>(online)</em>"}
+                            appleical: {show: true, text: "Apple Calendar"},
+                            google: {show: true, text: "Google <em>(online)</em>"},
+                            outlook: {show: true, text: "Outlook"},
+                            outlookcom: {show: true, text: "Outlook.com <em>(online)</em>"},
+                            yahoo: {show: false, text: "Yahoo <em>(online)</em>"}
                         });
                     };
                 };
@@ -40,9 +57,6 @@
                         self.isSmsLoading = false;
                     }, 2000);
                 }
-
-
-
             },
             templateUrl: 'src/app/components/steps/complete-step/complete-step.tpl.html',
             bindings: {
