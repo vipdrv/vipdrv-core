859006a800138cd5d2e9d8d3dd3934ed1d71b0c3
Ave Maria <ultramarine256@gmail.com>
2017-12-11 00:54:16 +0200

    Added migration;

--- src_api/QuantumLogic/QuantumLogic.Data/Migrations/20171210174106_AddedCarToLeadTable.Designer.cs
+++ src_api/QuantumLogic/QuantumLogic.Data/Migrations/20171210174106_AddedCarToLeadTable.Designer.cs
@@ -8,9 +8,10 @@ using QuantumLogic.Data.EFContext;
 namespace QuantumLogic.Data.Migrations
 {
     [DbContext(typeof(QuantumLogicDbContext))]
-    partial class QuantumLogicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171210174106_AddedCarToLeadTable")]
+    partial class AddedCarToLeadTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
---
+++ src_api/QuantumLogic/QuantumLogic.Data/Migrations/20171210174106_AddedCarToLeadTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuantumLogic.Data.Migrations
+{
+    public partial class AddedCarToLeadTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RouteId",
+                table: "Lead",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExpertId",
+                table: "Lead",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CarImageUrl",
+                table: "Lead",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CarTitle",
+                table: "Lead",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CarVin",
+                table: "Lead",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CarImageUrl",
+                table: "Lead");
+
+            migrationBuilder.DropColumn(
+                name: "CarTitle",
+                table: "Lead");
+
+            migrationBuilder.DropColumn(
+                name: "CarVin",
+                table: "Lead");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RouteId",
+                table: "Lead",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExpertId",
+                table: "Lead",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
