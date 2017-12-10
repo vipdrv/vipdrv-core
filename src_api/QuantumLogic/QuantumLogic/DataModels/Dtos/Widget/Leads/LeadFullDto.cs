3f61f8fdef1ff18cbd214730e7d41cafa14be2f5
Andrey Nevzorov <ya.kobos@yandex.ru>
2017-12-11 00:40:36 +0200

    Localization + performance improvement.

--- src_api/QuantumLogic/QuantumLogic/DataModels/Dtos/Widget/Leads/LeadFullDto.cs
+++ src_api/QuantumLogic/QuantumLogic/DataModels/Dtos/Widget/Leads/LeadFullDto.cs
@@ -1,29 +1,19 @@
-﻿using System;
+﻿using QuantumLogic.Core.Domain.Entities.WidgetModule;
+using System;
 
 namespace QuantumLogic.WebApi.DataModels.Dtos.Widget.Leads
 {
     public class LeadFullDto : LeadDto
     {
+        public string CarImageUrl { get; set; }
+
         #region Ctors
 
         public LeadFullDto() 
             : base()
         { }
 
-        public LeadFullDto(
-            int id,
-            int siteId,
-            int? expertId,
-            int? beverageId,
-            int? routeId,
-            string firstName,
-            string secondName,
-            string userPhone,
-            string userEmail,
-            string carImageUrl,
-            string carTitle,
-            string carVin,
-            DateTime bookingDateTimeUtc)
+        public LeadFullDto(int id, int siteId, int? expertId, int? beverageId, int? routeId, string firstName, string secondName, string userPhone, string userEmail, string carImageUrl, string carTitle, string carVin, DateTime bookingDateTimeUtc)
             : base(
                   id,
                   siteId,
@@ -34,11 +24,28 @@ namespace QuantumLogic.WebApi.DataModels.Dtos.Widget.Leads
                   secondName,
                   userPhone,
                   userEmail,
-                  carImageUrl,
                   carTitle,
                   carVin,
                   bookingDateTimeUtc)
-        { }
+        {
+            CarImageUrl = carImageUrl;
+        }
+
+        #endregion
+
+        #region Mapping
+
+        public override void MapFromEntity(Lead entity)
+        {
+            base.MapFromEntity(entity);
+            CarImageUrl = entity.CarImageUrl;
+        }
+        public override Lead MapToEntity()
+        {
+            Lead entity = base.MapToEntity();
+            entity.CarImageUrl = CarImageUrl;
+            return entity;
+        }
 
         #endregion
     }
