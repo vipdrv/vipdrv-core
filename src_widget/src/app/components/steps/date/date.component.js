1034494c3f027c3bf13f39085b844f79fd21df58
Ave Maria <ultramarine256@gmail.com>
2017-12-10 04:57:31 +0200

    Fixed date picker validation;

--- src_widget/src/app/components/steps/date/date.component.js
+++ src_widget/src/app/components/steps/date/date.component.js
@@ -4,25 +4,33 @@
             controller: function ($scope) {
                 var self = this;
 
+                self.isStepValid = false;
+                self.minDateString = moment().subtract(1, 'day').format('YYYY-MM-DD');
+
+                self.$onInit = function () {
+                    self.dateImput = self.userData.calendar.date;
+                    self.validateStep();
+                };
+
                 self.dateChanged = function (oldValue, newValue) {
                     var arr = newValue._i.split(' ');
-                    // var dayOfWeek = arr[1];
-
                     self.userData.calendar.date = arr[0];
+                    self.validateStep();
                 };
 
                 self.timeChanged = function (time) {
                     self.userData.calendar.time = time;
+                    self.validateStep();
                 };
 
-                this.$onInit = function () {
-                    self.openHours = 1;
-                };
+                self.timeIntervals = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 AM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM'];
 
-                self.isStepValid = true;
-                self.timeIntervals = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 AM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM'];
-
-                this.validateStep = function () {
+                self.validateStep = function () {
+                    if (self.userData.calendar.date != null && self.userData.calendar.time != null) {
+                        self.isStepValid = true;
+                    } else {
+                        self.isStepValid = false;
+                    }
                 };
 
                 $scope.nextStep = function () {
