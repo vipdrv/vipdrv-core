995ae09cce7a6986aae56594908e0f129a2a3a8a
Andrey Nevzorov <ya.kobos@yandex.ru>
2018-01-13 23:15:35 +0200

    DB migration for site wizard.

---
+++ src_api/QuantumLogic/QuantumLogic.Data/Migrations/20180113211102_SiteWizardSupport.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuantumLogic.Data.Migrations
+{
+    public partial class SiteWizardSupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UseBeverageStep",
+                table: "Site",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseExpertStep",
+                table: "Site",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseRouteStep",
+                table: "Site",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseBeverageStep",
+                table: "Site");
+
+            migrationBuilder.DropColumn(
+                name: "UseExpertStep",
+                table: "Site");
+
+            migrationBuilder.DropColumn(
+                name: "UseRouteStep",
+                table: "Site");
+        }
+    }
+}
