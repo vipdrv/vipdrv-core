c7c7d37a14cf7e8dc6d3e894d00ff5e7881a246c
Evgeny Platonov <ultramarine256@gmail.com>
2018-01-15 19:41:40 +0200

    Added limitation to event listener on VLP page;

--- src_admin/src/app/pages/pages.menu.ts
+++ src_admin/src/app/pages/pages.menu.ts
@@ -66,21 +66,22 @@ export const PAGES_MENU = [{
                             requiredPermission: 'CanViewMenuInvitations'
                         }
                     }
-                },
-                {
-                    path: 'users',
-                    data: {
-                        menu: {
-                            title: 'general.menu.users',
-                            icon: 'ion-android-folder-open',
-                            selected: false,
-                            expanded: false,
-                            order: 0,
-                            requiredPermission: 'CanViewMenuInvitations'
-                        }
-                    }
                 }
             ]
         }
     ]
 }];
+
+// {
+//     path: 'users',
+//         data: {
+//     menu: {
+//         title: 'general.menu.users',
+//             icon: 'ion-android-folder-open',
+//             selected: false,
+//             expanded: false,
+//             order: 0,
+//             requiredPermission: 'CanViewMenuUsers'
+//     }
+// }
+// }
