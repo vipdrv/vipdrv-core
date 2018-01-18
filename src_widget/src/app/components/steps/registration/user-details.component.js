40a8d9e3fa9caba1fe7032d09b6628536b7e5fb2
Evgeny Platonov <ultramarine256@gmail.com>
2018-01-19 00:02:44 +0200

    Beautified phone number input;

--- src_widget/src/app/components/steps/registration/user-details.component.js
+++ src_widget/src/app/components/steps/registration/user-details.component.js
@@ -2,25 +2,48 @@
     angular.module('myApp')
         .component('tdUserDetails', {
             controller: function ($scope, $window, api, dealerData, bookingData) {
-
                 var self = this;
+
                 self.dealerData = dealerData;
                 self.bookingData = bookingData;
 
-                self.$onInit = function() {
+                self.$onInit = function () {
                     _makeWidgetRootScrollable();
+                    $window.$("#userPhone").intlTelInput({
+                        // allowDropdown: false,
+                        // autoHideDialCode: false,
+                        // autoPlaceholder: "off",
+                        // dropdownContainer: "body",
+                        // excludeCountries: ["us"],
+                        // formatOnDisplay: false,
+                        geoIpLookup: function (callback) {
+                            $window.$.get("http://ipinfo.io", function () {
+                            }, "jsonp").always(function (resp) {
+                                var countryCode = (resp && resp.country) ? resp.country : "";
+                                callback(countryCode);
+                            });
+                        },
+                        // hiddenInput: "full_number",
+                        initialCountry: "auto",
+                        // nationalMode: false,
+                        // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+                        // placeholderNumberType: "MOBILE",
+                        // preferredCountries: ['cn', 'jp'],
+                        // separateDialCode: true,
+                        // utilsScript: "build/js/utils.js"
+                    });
                 };
 
                 self.makeBooking = function () {
                     self.bookingData.user.firstName = $scope.firstName;
                     self.bookingData.user.lastName = $scope.secondName;
                     self.bookingData.user.email = $scope.email;
-                    self.bookingData.user.phone = $scope.phone;
+                    self.bookingData.user.phone = $window.$("#userPhone").intlTelInput("getNumber");
                     self.bookingData.user.comment = $scope.comment;
 
                     api.completeBooking(self.bookingData).then(function () {
                     });
-                    // TODO: add reaction on promise
+
                     self.completeForm();
                 };
 
@@ -30,6 +53,24 @@
                         div.style.display = 'table';
                     }
                 };
+
+                /* Methods */
+
+                /* Validation */
+                document.getElementById('userPhone').addEventListener('input', function (element) {
+                    phoneNumberImputMask(element);
+                });
+
+                var userPhone = $window.$("#userPhone");
+                var userPhoneSelector = $window.document.getElementById('userPhone');
+                userPhone.on("keyup change", function () {
+                    if (userPhone.val().length == 14) {
+                        userPhoneSelector.style.border = '1px solid #28a745';
+                    } else {
+                        // userPhoneSelector.style.border = '1px solid #dc3545';
+                    }
+                });
+
             },
             templateUrl: 'src/app/components/steps/registration/user-details.tpl.html',
             bindings: {
