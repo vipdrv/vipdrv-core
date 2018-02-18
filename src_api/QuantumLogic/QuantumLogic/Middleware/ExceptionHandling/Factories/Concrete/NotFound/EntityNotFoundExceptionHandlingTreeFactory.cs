6720be648b2d51a2824914c1664235f0dc957ff8
Andrey Nevzorov <ya.kobos@yandex.ru>
2018-02-19 01:34:17 +0200

    Added EntityNotFoundException handling via middleware.

---
+++ src_api/QuantumLogic/QuantumLogic/Middleware/ExceptionHandling/Factories/Concrete/NotFound/EntityNotFoundExceptionHandlingTreeFactory.cs
@@ -0,0 +1,14 @@
+﻿using QuantumLogic.WebApi.Middleware.ExceptionHandling.Handlers;
+using QuantumLogic.WebApi.Middleware.ExceptionHandling.Handlers.Concrete.NotFound;
+
+namespace QuantumLogic.WebApi.Middleware.ExceptionHandling.Factories.Concrete.NotFound
+{
+    public class EntityNotFoundExceptionHandlingTreeFactory : IExceptionHandlingTreeFactory
+    {
+        public IExceptionHandler CreateExceptionHandlingTree()
+        {
+            IExceptionHandler exceptionHandlingTree = new EntityNotFoundExceptionHandler();
+            return exceptionHandlingTree;
+        }
+    }
+}
---
+++ src_api/QuantumLogic/QuantumLogic/Middleware/ExceptionHandling/Handlers/Concrete/NotFound/EntityNotFoundExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using QuantumLogic.Core.Exceptions.NotFound;
+using QuantumLogic.WebApi.DataModels.Dtos.Shared.Error;
+using System;
+using System.Net;
+
+namespace QuantumLogic.WebApi.Middleware.ExceptionHandling.Handlers.Concrete.NotFound
+{
+    public class EntityNotFoundExceptionHandler : AbstractExceptionHandler<EntityNotFoundException>
+    {
+        public override int StatusCode
+        {
+            get
+            {
+                return (int)HttpStatusCode.NotFound;
+            }
+        }
+
+        public override bool CanHandle(HttpContext context, Exception ex)
+        {
+            return ex is EntityNotFoundException;
+        }
+
+        protected override ErrorDto CreateErrorDto(HttpContext context, EntityNotFoundException ex)
+        {
+            return new ErrorDto(context.TraceIdentifier, $"Entity not found! {ex.Message}");
+        }
+    }
+}
