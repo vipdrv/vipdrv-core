88627339644508df969a8e5b89471f7814eff270
Andrey Nevzorov <ya.kobos@yandex.ru>
2018-03-19 01:06:15 +0200

    Added data migration script

---
+++ src_api/QuantumLogic/Scripts/AddStepsDataMigration.sql
@@ -0,0 +1,24 @@
+USE [database_name]
+GO
+
+INSERT INTO [dbo].[Step] ([Descriptor] ,[IsActive] ,[Name] ,[Order] ,[SiteId])
+SELECT 'ScheduleStep' as Descriptor, 1 as IsActive, 'ScheduleStep' as [Name], 1 as [Order], Id as SiteId
+FROM [dbo].[Site]
+
+INSERT INTO [dbo].[Step] ([Descriptor] ,[IsActive] ,[Name] ,[Order] ,[SiteId])
+SELECT 'ExpertStep' as Descriptor, [UseExpertStep] as IsActive, 'ExpertStep' as [Name], ExpertStepOrder + 1 as [Order], Id as SiteId
+FROM [dbo].[Site]
+
+INSERT INTO [dbo].[Step] ([Descriptor] ,[IsActive] ,[Name] ,[Order] ,[SiteId])
+SELECT 'BeverageStep' as Descriptor, UseBeverageStep as IsActive, 'BeverageStep' as [Name], BeverageStepOrder + 1 as [Order], Id as SiteId
+FROM [dbo].[Site]
+
+INSERT INTO [dbo].[Step] ([Descriptor] ,[IsActive] ,[Name] ,[Order] ,[SiteId])
+SELECT 'RouteStep' as Descriptor, [UseRouteStep] as IsActive, 'RouteStep' as [Name], RouteStepOrder + 1 as [Order], Id as SiteId
+FROM [dbo].[Site]
+
+INSERT INTO [dbo].[Step] ([Descriptor] ,[IsActive] ,[Name] ,[Order] ,[SiteId])
+SELECT 'MusicStep' as Descriptor, 0 as IsActive, 'MusicStep' as [Name], 5 as [Order], Id as SiteId
+FROM [dbo].[Site]
+
+GO
